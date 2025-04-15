93f8829729fdfe1ff55109d373fe599a8d8a0c10
Bianca Lucas <biaxx.iluks@gmail.com>
2025-04-15 16:36:43 -0300

    feat: get all users end

--- app.js
+++ app.js
@@ -2,8 +2,30 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/users', (req, res) => {
-    res.send('Olá usuário!')
+// -> permitir ler JSON no corpo da requisição
+app.use(express.json())
+
+// -> Banco de Dados in memory
+const usuarios = [
+    { id: 1, nome: 'João', email: 'joao@email.com' },
+    { id: 2, nome: 'Ana', email: 'ana@email.com' }
+]
+
+app.get('/', (req, res) => {
+    res.send('Bem vindo a API!')
+})
+
+app.get('/usuario', (req, res) => {
+    res.send(usuarios)
+})
+
+app.post("/criarUsuario", (req, res) => {
+    const { nome, sobrenome } = req.body;
+    res.send(`
+        
+        Nome Completo é: ${nome} ${sobrenome}
+
+        `)
 })
 
 app.listen(port, () => {
