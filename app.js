4f07196afc9eabbee5cdfd1ebf49b39c4175f9c4
Bianca Lucas <biaxx.iluks@gmail.com>
2025-04-22 16:17:03 -0300

    refact|feat: usuários e fornecedores

--- app.js
+++ app.js
@@ -1,32 +1,23 @@
 import express from 'express'
+import usuariosRoutes from './routes/user.js'
+import fornecedoresRoutes from './routes/fornecedores.js'
+
 const app = express()
 const port = 3000
 
 // -> permitir ler JSON no corpo da requisição
 app.use(express.json())
 
-// -> Banco de Dados in memory
-const usuarios = [
-    { id: 1, nome: 'João', email: 'joao@email.com' },
-    { id: 2, nome: 'Ana', email: 'ana@email.com' }
-]
-
+// -> Home:
 app.get('/', (req, res) => {
-    res.send('Bem vindo a API!')
+    res.send('Bem-vindo a API!')
 })
 
-app.get('/usuario', (req, res) => {
-    res.send(usuarios)
-})
+// -> Adiciona o roteador 
+app.use("/usuarios", usuariosRoutes)
 
-app.post("/criarUsuario", (req, res) => {
-    const { nome, sobrenome } = req.body;
-    res.send(`
-        
-        Nome Completo é: ${nome} ${sobrenome}
-
-        `)
-})
+// -> Router: Fornecedores
+app.use("/fornecedores", fornecedoresRoutes)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
